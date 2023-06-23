9c0538e81899f8b878971694eb1e9d0376204d92
Nicolas Oulianov <nicolas.oulianov@gmail.com>
2023-06-23 17:03:36 +0200

    Lesson about converting error types

--- src/http/request.rs
+++ src/http/request.rs
@@ -2,6 +2,8 @@ use super::method::Method;
 use std::convert::TryFrom;
 use std::error::Error;
 use std::fmt::{Debug, Display, Formatter, Result as FmtResult};
+use std::str;
+use std::str::Utf8Error;
 
 pub struct Request {
     path: String,
@@ -21,8 +23,28 @@ pub struct Request {
 impl TryFrom<&[u8]> for Request {
     type Error = ParseError;
 
-    fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
+    fn try_from(buffer: &[u8]) -> Result<Self, Self::Error> {
         // If not implement and want to add a WIP, use the unimplemented! macro
+        // unimplemented!()
+
+        // match str::from_utf8(buffer) {
+        //     Ok(request) => {}
+        //     Err(_) => return Err(ParseError::InvalidEncoding),
+        // }
+
+        // another common pattern for error handling is this one
+        // match str::from_utf8(buffer).or(Err(ParseError::InvalidEncoding)){
+        //     Ok(request) => {};
+        //     Err(e) => return Err(e),
+        // }
+
+        // This pattern of error handling is common, so a shortcut is just:
+        // let request = str::from_utf8(buffer).or(Err(ParseError::InvalidEncoding))?;
+
+        // Actually, since the type of Error is set into the object, the default error will
+        // be converted to a ParseError using the .from_type implementation, so we can just do:
+        let request = str::from_utf8(buffer)?;
+
         unimplemented!()
     }
 
@@ -45,6 +67,12 @@ impl ParseError {
             Self::InvalidProtocol => "InvalidProtocol",
             Self::InvalidMethod => "InvalidMethod",
         }
+    }
+}
+
+impl From<Utf8Error> for ParseError {
+    fn from(_: Utf8Error) -> Self {
+        Self::InvalidEncoding
     }
 }
 
